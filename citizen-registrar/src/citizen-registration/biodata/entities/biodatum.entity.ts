11d74a252289826421232319b9cde206490459ba
glory-fola1 <damilolafolami2002@gmail.com>
2022-03-04 15:00:08 +0100

    update

--- citizen-registrar/src/citizen-registration/biodata/entities/biodatum.entity.ts
+++ citizen-registrar/src/citizen-registration/biodata/entities/biodatum.entity.ts
@@ -1,1 +1,40 @@
-export class Biodatum {}
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class Biodatum {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column({ nullable: true })
+    middleName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    dateOfBirth: Date;
+
+    @Column()
+    nationality: string;
+
+    @Column()
+    countryOfBirth: string;
+
+    @Column({ nullable: true })
+    stateOfBirth: string;
+
+    @Column({ nullable: true })
+    townOfBirth: string;
+
+    @Column({ nullable: true })
+    residenceAddress: string;
+
+    @Column()
+    profession: string;
+    LinkedIdentity: any;
+
+}
