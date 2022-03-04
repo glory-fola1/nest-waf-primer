66017e624703da97fc513ea5e28b331d1ed84486
glory-fola1 <damilolafolami2002@gmail.com>
2022-03-04 14:59:24 +0100

    update

--- citizen-registrar/src/app.module.ts
+++ citizen-registrar/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CitizenRegistrationModule } from './citizen-registration/citizen-registration.module';
 
 @Module({
-  imports: [CitizenRegistrationModule],
+  imports: [TypeOrmModule.forRoot(), CitizenRegistrationModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
+320
